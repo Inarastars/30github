bd2a115ca9f647b5c631c0a41714fa66ab27ee59
Inarastars <inara.aksha@gmail.com>
2025-02-28 17:02:13 +0500

    задачи на пайтоне
    решение задач на пайтоне, для подготовки к экзамену

---
+++ informatica/main.py
@@ -0,0 +1,39 @@
+for i in range(4):
+    print(3)
+
+for i in range(3):
+    print(2)
+
+for i in range(5):
+    print(10, end=" ")
+
+for i in range(7):
+    print(7, end=" ")
+
+n = int(input("введите число n:"))
+for i in range(n):
+    print(9, end=" ")
+
+n = int(input("введите число n:"))
+for i in range(5):
+    print(n, end=" ")
+
+for i in range(2,6):
+    print(i)
+
+for i in range(11,21):
+    print(i)
+
+b = int(input("введите число b:"))
+for i in range(2,b + 1):
+    print(i)
+
+for i in range(1,10,2):
+    print(i, end=" ")
+
+for i in range(45,67,3):
+    print(i, end=" ")
+
+n = int(input("введите колво чисел"))
+for i in range(1,1+n):
+    print(i**2+1 , end=" ")
