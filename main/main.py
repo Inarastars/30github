20d4353fd03a81c67c186f25a0c674b398801388
Inarastars <inara.aksha@gmail.com>
2025-03-07 17:09:17 +0500

    Add files via upload
    тренировка с циклом for i in range

---
+++ main/main.py
@@ -0,0 +1,12 @@
+for i in range(1,11):
+    print(i)
+
+for i in range(2,21,2):
+    print(i)
+
+for i in range(11, 1 , -1):
+    print(i)
+
+for i in range(1,11):
+    for j in range(1,11):
+        print(f"{i} * {j}")
